77cd14430347202736221c567ff08742c930a472
Kyle Cardiel <kyle.cardiel@gmail.com>
2020-08-15 19:02:11 -0500

    collapsible assortment feature

--- src/components/blackSeries/assortmentHeader.js
+++ src/components/blackSeries/assortmentHeader.js
@@ -2,12 +2,19 @@ import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { HeaderText } from 'components/common/text/headerText';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import { Color } from 'shared/styles/color';
 
-export const AssortmentHeader = ({ text, backgroundColor }) => {
+export const AssortmentHeader = ({ text, backgroundColor, onViewChange, view }) => {
     const classes = useStyles({ backgroundColor });
+    const icon = view ? <ExpandLessIcon fontSize='large'/> : <ExpandMoreIcon fontSize='large'/>;
     return (
         <Grid container spacing={1} className={classes.headerContainer}>
-            <Grid item xs={12} >
+            <Grid item xs={1} onClick={onViewChange}>
+                <div className={classes.button}>{icon}</div>
+            </Grid>
+            <Grid item xs={10} >
                 <HeaderText text={text} />
             </Grid>
         </Grid>
@@ -19,7 +26,17 @@ const useStyles = makeStyles(theme => ({
         marginTop: theme.spacing(2),
         paddingTop: theme.spacing(1),
         paddingBottom: theme.spacing(1),
+        paddingLeft: theme.spacing(1),
         width: '100%',
         background: props => props.backgroundColor,
+    },
+    button: {
+        borderRadius: 100,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        '&:hover': {
+            backgroundColor: Color.white(),
+        },
     },
 }));
