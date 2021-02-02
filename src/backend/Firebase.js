f486993ca6b74996ee12ce9bbf1e52c1ee395042
Kyle Cardiel <kyle.cardiel@gmail.com>
2021-02-01 21:11:35 -0600

    lint fixes

--- src/backend/Firebase.js
+++ src/backend/Firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbGh8-CDP-VHoXtseYvSFuGAwXh_Z8dL8",
-  authDomain: "swcollection2187.firebaseapp.com",
-  databaseURL: "https://swcollection2187.firebaseio.com",
-  projectId: "swcollection2187",
-  storageBucket: "swcollection2187.appspot.com",
-  messagingSenderId: "659788578698",
-  appId: "1:659788578698:web:8e0bbdfb3b87be6db49273"
+    apiKey: 'AIzaSyDbGh8-CDP-VHoXtseYvSFuGAwXh_Z8dL8',
+    authDomain: 'swcollection2187.firebaseapp.com',
+    databaseURL: 'https://swcollection2187.firebaseio.com',
+    projectId: 'swcollection2187',
+    storageBucket: 'swcollection2187.appspot.com',
+    messagingSenderId: '659788578698',
+    appId: '1:659788578698:web:8e0bbdfb3b87be6db49273',
 };
 
 firebase.initializeApp(firebaseConfig);
--- src/backend/FirebaseAuth.js
+++ src/backend/FirebaseAuth.js
@@ -5,46 +5,46 @@ export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
 
 export const onAuthStateChange = callback => {
-  return auth.onAuthStateChanged(user => {
-    if (user) {
-      callback({
-        loggedIn: true,
-        email: user.email,
-        id: user.uid,
-      });
-    } else {
-      callback(defaultUser);
-    }
-  });
+    return auth.onAuthStateChanged(user => {
+        if (user) {
+            callback({
+                loggedIn: true,
+                email: user.email,
+                id: user.uid,
+            });
+        } else {
+            callback(defaultUser);
+        }
+    });
 };
 
 export const login = (email, password) => {
-  auth.signInWithEmailAndPassword(email, password)
-    .catch(error => {
-      if (error.message !== null) {
-        alert(error.message);
-      }
-    })
+    auth.signInWithEmailAndPassword(email, password)
+        .catch(error => {
+            if (error.message !== null) {
+                alert(error.message);
+            }
+        });
 };
 
 export const logout = () => {
-  auth.signOut();
+    auth.signOut();
 };
 
 export const registerUser = registrationInfo => {
-  auth.createUserWithEmailAndPassword(
-    registrationInfo.email,
-    registrationInfo.password
-  ).then(() => {
-    auth.onAuthStateChanged(user => {
-      user.updateProfile({
-        email: registrationInfo.email,
-      })
+    auth.createUserWithEmailAndPassword(
+        registrationInfo.email,
+        registrationInfo.password,
+    ).then(() => {
+        auth.onAuthStateChanged(user => {
+            user.updateProfile({
+                email: registrationInfo.email,
+            });
+        });
     })
-  })
-    .catch(error => {
-      if (error.message !== null) {
-        alert(error.message);
-      }
-    })
+        .catch(error => {
+            if (error.message !== null) {
+                alert(error.message);
+            }
+        });
 };
