626aa9764b335ec857a548e90a8f73322200c80d
Kyle Cardiel <kyle.cardiel@gmail.com>
2021-02-15 15:43:10 -0600

    added env file and package

--- src/backend/Firebase.js
+++ src/backend/Firebase.js
@@ -4,13 +4,14 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDbGh8-CDP-VHoXtseYvSFuGAwXh_Z8dL8',
-    authDomain: 'swcollection2187.firebaseapp.com',
-    databaseURL: 'https://swcollection2187.firebaseio.com',
-    projectId: 'swcollection2187',
-    storageBucket: 'swcollection2187.appspot.com',
-    messagingSenderId: '659788578698',
-    appId: '1:659788578698:web:8e0bbdfb3b87be6db49273',
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
