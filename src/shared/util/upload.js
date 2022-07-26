89f19ee8e87b7b59df9e5baade74ff7539502641
Kyle Cardiel <kyle.cardiel@gmail.com>
2022-07-26 13:37:18 -0500

    fix upload to storage file

--- src/shared/util/upload.js
+++ src/shared/util/upload.js
@@ -1,8 +1,9 @@
 import { storage } from 'backend/Firebase';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 export const uploadImageToStorage = async (location, image, setPercentage) => {
     return new Promise((resolve, reject) => {
-        const uploadTask = storage.ref(location).put(image);
+        const uploadTask = uploadBytesResumable(ref(storage, location), image);
         uploadTask.on('state_changed',
             snapshot => {
                 let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
@@ -13,7 +14,7 @@ export const uploadImageToStorage = async (location, image, setPercentage) => {
                 reject();
             },
             function complete() {
-                uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                     resolve(downloadURL);
                 });
             },
